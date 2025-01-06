f594e7bec47c88c2d53df346a974a523c38e2df5
heejune1209 <heejune1209@gmail.com>
2025-01-06 16:26:23 +0900

    right rule

---
+++ Right_Hand_Rule/Maze/Player.cpp
@@ -0,0 +1,93 @@
+#include "pch.h"
+#include "Player.h"
+#include "Board.h"
+
+void Player::Init(Board* board)
+{
+	_pos = board->GetEnterPos();
+	_board = board;
+
+	Pos pos = _pos;
+
+	_path.clear();
+	_path.push_back(pos);
+
+	// 목적지 도착하기 전에는 계속 실행
+	Pos dest = board->GetExitPos();
+
+	// 다음 방향 포지션 찾기
+	// Pos는 (y, x)의 값을 가지는 구조체
+	Pos front[4] =
+	{
+		Pos {-1,0}, // UP
+		Pos {0,-1}, // LEFT
+		Pos {1,0}, // Down
+		Pos {0,1}, // Right
+	};
+
+	while (pos != dest)
+	{
+		// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈수있는지 확인.
+		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
+		if (CanGo(pos + front[newDir]))
+		{
+			// 오른쪽 방향으로 90도 회전.
+			_dir = newDir;
+			// 앞으로 한 보 전진
+			pos += front[_dir];
+			// 이동한 좌표를 저장
+			_path.push_back(pos);
+		}
+		// 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
+		else if (CanGo(pos +front[_dir]))
+		{
+			// 앞으로 한 보 전진
+			pos += front[_dir];
+			_path.push_back(pos);
+		}
+		else
+		{
+			// 왼쪽 방향으로 90도 회전.
+			// 아래에 스위치 문으로 만들수 있는것을 이렇게 한줄로 만들어버릴수 있음
+			_dir = (_dir + 1) % DIR_COUNT;
+				
+			/*switch (_dir)
+			{
+			case DIR_UP:
+				_dir = DIR_LEFT;
+				break;
+			case DIR_LEFT:
+				_dir = DIR_DOWN;
+				break;
+			case DIR_DOWN:
+				_dir = DIR_RIGHT;
+				break;
+			case DIR_RIGHT:
+				_dir = DIR_UP;
+				break;
+			}*/
+		}
+	}
+}
+
+void Player::Update(uint64 deltaTick)
+{
+	// 일정 시간마다 경로 업데이트
+	if (_pathIndex >= _path.size())
+		return;
+	
+	_sumTick += deltaTick;
+	if (_sumTick >= MOVE_TICK)
+	{
+		_sumTick = 0;
+
+		_pos = _path[_pathIndex];
+		_pathIndex++;
+	}
+}
+
+bool Player::CanGo(Pos pos)
+{
+	TileType tileType = _board->GetTileType(pos);
+	return tileType == TileType::EMPTY;
+}
--- Right_Hand_Rule/Maze/Player.h
+++ Right_Hand_Rule/Maze/Player.h
@@ -4,6 +4,12 @@ class Board;
 
 class Player
 {
+	enum 
+	{
+		// 0.1초 단위로 이동시켜주기 위해 MOVE_TICK을 100으로 설정
+		MOVE_TICK = 100,
+	};
+
 public:
 	void		Init(Board* board);
 	void		Update(uint64 deltaTick);
@@ -11,10 +17,15 @@ public:
 	void		SetPos(Pos pos) { _pos = pos; }
 	Pos			GetPos() { return _pos; }
 
-
+	bool CanGo(Pos pos);
 private:
 	Pos			_pos = {};
 	int32		_dir = DIR_UP;
 	Board* _board = nullptr;
+
+	vector<Pos> _path;
+	// 경로를 기준으로 어디까지 이동했는지 추적하는 용도
+	uint32 _pathIndex = 0;
+	uint64 _sumTick = 0;
 };
 
