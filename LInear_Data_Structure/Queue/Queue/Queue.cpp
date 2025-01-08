1f8a4b3634c7e0c68abfb6eb633f599c2899805d
heejune1209 <144242233+heejune1209@users.noreply.github.com>
2025-01-08 10:43:26 +0900

    queue

--- LInear_Data_Structure/Queue/Queue/Queue.cpp
+++ LInear_Data_Structure/Queue/Queue/Queue.cpp
@@ -1,20 +1,122 @@
-﻿// Queue.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
 
-#include <iostream>
+// Queue (FIFO First-in-First-out 선입선출)
+
+// front << [1][2][3][4] << rear(back)
+// 예시) 대기열
+
+template<typename T>
+class ArrayQueue // 순환 구조 큐를 이용
+{
+public:
+    ArrayQueue()
+    {
+        //_container.resize(100);
+    }
+    void push(const T& value)
+    {
+        // TODO : 다 찼는지 체크
+        if (_size == _container.size())
+        {
+            // 증설 작업
+            int newSize = max(1, _size * 2); // max 함수는 둘 중 큰 값을 리턴
+            vector<T> newData;
+            newData.resize(newSize);
+
+            // 데이터 복사
+            for (int i = 0; i < _size; i++)
+            {
+                int index = (_front + i) % _container.size();
+                newData[i] = _container[index];
+            }
+            // 교체
+            _container.swap(newData);
+            _front = 0;
+            _back = _size;
+        }
+
+        _container[_back] = value;
+        // 나머지 연산으로 빠져나가지 않게 범위 안에 돌게 한다
+        _back = (_back + 1) % _container.size();
+        _size++;
+    }
+    void pop()
+    {
+        _front = (_front + 1) % _container.size();
+        _size--;
+    }
+    T& front()
+    {
+        return _container[_front];
+    }
+    bool empty()
+    {
+        return _size == 0;
+    }
+    int size()
+    {
+        return _size;
+    }
+private:
+    vector<T> _container;
+    
+    int _front = 0;
+    int _back = 0;
+    int _size = 0;
+
+};
+
+//template<typename T>
+//class ListQueue
+//{
+//public:
+//    void push(const T& value)
+//    {
+//        _container.push_back(value);
+//    }
+//    void pop()
+//    {
+//        // 효율이 좋지 않다. (동적 배열에서의 앞 요소 추가 및 삭제)
+//        // _container.erase(_container.begin());
+//        // 리스트 사용
+//        _container.pop_front();
+//    }
+//    T& front()
+//    {
+//        return _container.front();
+//    }
+//    bool empty()
+//    {
+//        return _container.empty();
+//    }
+//    int size()
+//    {
+//        return _container.size();
+//    }
+//private:
+//    list<T> _container;
+//    // 덱(deque)을 사용해도 리스트와 비슷한 속도로 동작된다.(원래 코드는 덱으로 되어있음)
+//};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //ListQueue<int> q;
+    ArrayQueue<int> q;
+    for (int i = 0; i < 100; i++)
+    {
+        q.push(i);
+    }
+    while (q.empty() == false)
+    {
+        int value = q.front();
+        q.pop();
+        cout << value << endl;
+    }
+    int size = q.size();
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
