c0eaeb9b5bf7e50c0926181e6bf1a45c762ebde9
heejune1209 <heejune1209@gmail.com>
2025-01-26 18:09:09 +0900

    com

--- Minimum_Spanning_Tree/Prim_Algoritm/Prim_Algoritm/Prim_Algoritm.cpp
+++ Minimum_Spanning_Tree/Prim_Algoritm/Prim_Algoritm/Prim_Algoritm.cpp
@@ -4,6 +4,15 @@
 #include <climits> // INT_MAX 사용
 using namespace std;
 
+// 프림 알고리즘은  최소 신장 트리(MST, Minimum Spanning Tree)를 찾기 위한 대표적인 알고리즘 중 하나이다. 
+// MST는 연결된 가중치 그래프에서 모든 정점을 포함하면서 가중치 합이 최소가 되는 트리를 의미.
+
+// 프림 알고리즘의 순서
+// 1. 시작 정점을 선택하고, 이를 MST에 포함.
+// 2. MST에 포함된 정점과 포함되지 않은 정점 사이의 가장 작은 가중치를 가진 간선을 선택.
+// 3. 선택된 간선의 정점을 MST에 추가.
+// 4. 모든 정점이 MST에 포함될 때까지 위 과정을 반복.
+
 // 간선을 표현하는 구조체
 struct Edge 
 {
@@ -18,6 +27,33 @@ struct Edge
         return cost > e.cost;
     }
 };
+vector<vector<Edge>> graph;
+
+void CreateGraph()
+{
+    graph.resize(6);
+
+    // 그래프 간선 추가 (양방향 그래프), 인접 리스트를 활용
+    graph[0].push_back(Edge(1, 15));
+    graph[0].push_back(Edge(3, 35));
+
+    graph[1].push_back(Edge(0, 15));
+    graph[1].push_back(Edge(2, 5));
+    graph[1].push_back(Edge(3, 10));
+
+    graph[2].push_back(Edge(1, 5));
+
+    graph[3].push_back(Edge(0, 35));
+    graph[3].push_back(Edge(1, 10));
+    graph[3].push_back(Edge(4, 5));
+    graph[3].push_back(Edge(5, 10));
+
+    graph[4].push_back(Edge(3, 5));
+    graph[4].push_back(Edge(5, 5));
+
+    graph[5].push_back(Edge(3, 10));
+    graph[5].push_back(Edge(4, 5));
+}
 
 // 프림 알고리즘
 int primMST(int n, vector<vector<Edge>>& graph) 
@@ -65,30 +101,11 @@ int primMST(int n, vector<vector<Edge>>& graph)
 
 int main() 
 {
-    int n = 5; // 정점 개수
-    vector<vector<Edge>> graph(n);
-
-    // 그래프 간선 추가 (양방향 그래프), 인접 리스트를 활용
-    graph[0].push_back(Edge(1, 2));
-    graph[1].push_back(Edge(0, 2));
-
-    graph[0].push_back(Edge(3, 3));
-    graph[3].push_back(Edge(0, 3));
-
-    graph[1].push_back(Edge(2, 1));
-    graph[2].push_back(Edge(1, 1));
-
-    graph[1].push_back(Edge(3, 4));
-    graph[3].push_back(Edge(1, 4));
-
-    graph[2].push_back(Edge(4, 5));
-    graph[4].push_back(Edge(2, 5));
-
-    graph[3].push_back(Edge(4, 6));
-    graph[4].push_back(Edge(3, 6));
+    // 그래프 생성
+    CreateGraph();
 
     // MST의 최소 가중치 출력
-    int cost = primMST(n, graph);
+    int cost = primMST(6, graph);
     cout << "최소 신장 트리의 가중치: " << cost << endl;
 
     return 0;
